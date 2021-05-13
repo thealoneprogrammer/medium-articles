595d339c5a6edbb31d374d37391026e2789e437f
thealoneprogrammer <sujithsuji1098@gmail.com>
2021-05-13 23:21:05 +0530

    1.0.1

--- src/__tests__/mediumPosts.test.ts
+++ src/__tests__/mediumPosts.test.ts
@@ -1,17 +1,20 @@
 import { getMediumPosts } from '../index';
 
 test('Valid username', async () => {
-    expect(typeof (await getMediumPosts('nirmaan_iitm'))).toBe('object');
+  expect(typeof (await getMediumPosts('nirmaan_iitm'))).toBe('object');
 });
 
 test('Empty string', async () => {
-    expect(await getMediumPosts('')).toBe('Username cannot be empty');
+  expect(await getMediumPosts('')).toBe('Username cannot be empty');
 });
 
 test('Prefixed @', async () => {
-    expect(typeof await getMediumPosts('@nirmaan_iitm')).toBe("object");
+  expect(typeof (await getMediumPosts('@nirmaan_iitm'))).toBe('object');
 });
 
 test('Invalid username', async () => {
-    expect(await getMediumPosts('hshsyududud/87')).toStrictEqual({ status: 404, message: 'Invalid username or posts not found for this username' });
+  expect(await getMediumPosts('hshsyududud/87')).toStrictEqual({
+    status: 404,
+    message: 'Invalid username or posts not found for this username',
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,18 @@ import { BASE_URL } from './constants';
 const parser: Parser = new Parser();
 
 export const getMediumPosts = async (username: string) => {
-    if (username === '') return 'Username cannot be empty';
+  if (username === '') return 'Username cannot be empty';
 
-    const URL = prepareURL(username);
-    try {
-        const feed = await parser.parseURL(URL);
-        return { status: 200, message: 'success', posts: feed.items };
-    } catch (error) {
-        return { status: 404, message: 'Invalid username or posts not found for this username' };
-    }
+  const URL = prepareURL(username);
+  try {
+    const feed = await parser.parseURL(URL);
+    return { status: 200, message: 'success', posts: feed.items };
+  } catch (error) {
+    return { status: 404, message: 'Invalid username or posts not found for this username' };
+  }
 };
 
 const prepareURL = (username: string): string => {
-    if (username.trim().charAt(0) === '@') username = username.substring(1);
-    return `${BASE_URL}/@${username}`;
+  if (username.trim().charAt(0) === '@') username = username.substring(1);
+  return `${BASE_URL}/@${username}`;
 };
