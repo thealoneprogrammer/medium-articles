3fa6419cf6a0acb6d128f6f305e54d0478aa1536
thealoneprogrammer <sujithsuji1098@gmail.com>
2021-05-13 23:11:34 +0530

    initial version release

---
+++ src/__tests__/mediumPosts.test.ts
@@ -0,0 +1,17 @@
+import { getMediumPosts } from '../index';
+
+test('Valid username', async () => {
+    expect(typeof (await getMediumPosts('nirmaan_iitm'))).toBe('object');
+});
+
+test('Empty string', async () => {
+    expect(await getMediumPosts('')).toBe('Username cannot be empty');
+});
+
+test('Prefixed @', async () => {
+    expect(typeof await getMediumPosts('@nirmaan_iitm')).toBe("object");
+});
+
+test('Invalid username', async () => {
+    expect(await getMediumPosts('hshsyududud/87')).toStrictEqual({ status: 404, message: 'Invalid username or posts not found for this username' });
+});
